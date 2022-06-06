59d18e7c493ceaab6926b511c1bb884295554530
Maxim Rachinskiy <myurachinskiy@edu.hse.ru>
2022-06-06 19:55:36 +0300

    cifar100

--- results_reproduce/zero_shot.py
+++ results_reproduce/zero_shot.py
@@ -1,22 +1,38 @@
 import os
+import importlib
 
+import fire
 import clip
 import torch
 import torch.utils
 from tqdm import tqdm
-from torchvision.datasets import CIFAR100
 
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load('ViT-L/14@336px', device)
+def get_dataset(dataset_name, preprocess):
+    root = os.path.expanduser("~/.cache")
+    if dataset_name == 'CIFAR100':
+        from torchvision.datasets import CIFAR100
+        return CIFAR100(root=root, train=False, transform=preprocess)
+    elif dataset_name == 'ImageNet':
+        from torchvision.datasets import ImageNet
+        return ImageNet(root=root, split='val', transform=preprocess)
+    elif dataset_name == 'CIFAR10':
+        from torchvision.datasets import CIFAR10
+        return CIFAR10(root=root, train=False, transform=preprocess)
+    raise ValueError(f"Unsupported dataset: {dataset_name}")
 
-cifar100 = CIFAR100(root=os.path.expanduser("~/.cache"), train=False, transform=preprocess)
-loader = torch.utils.data.DataLoader(cifar100, batch_size=32, num_workers=2)
 
-cifar100_classes = cifar100.classes
-cifar100_templates = ["a photo of a {}"]
+def load_promts(dataset_name):
+    promts_mapping = {'CIFAR100': 'cifar100'}
+    if dataset_name not in promts_mapping:
+        raise ValueError("Unsupported dataset for promts: {dataset_name}")
 
-def zeroshot_classifier(classnames, templates):
+    module_name = promts_mapping[dataset_name]
+    module = importlib.import_module(f'results_reproduce.promts.{module_name}')
+    return module.classes, module.templates
+
+
+def zeroshot_classifier(model, classnames, templates):
     with torch.no_grad():
         zeroshot_weights = []
         for classname in tqdm(classnames):
@@ -31,35 +47,46 @@ def zeroshot_classifier(classnames, templates):
     return zeroshot_weights
 
 
-zeroshot_weights = zeroshot_classifier(cifar100_classes, cifar100_templates)
-
-
 def accuracy(output, target, topk=(1,)):
     pred = output.topk(max(topk), 1, True, True)[1].t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
     return [float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy()) for k in topk]
 
 
-with torch.no_grad():
-    top1, top5, n = 0., 0., 0.
-    for i, (images, target) in enumerate(tqdm(loader)):
-        images = images.cuda()
-        target = target.cuda()
-        
-        # predict
-        image_features = model.encode_image(images)
-        image_features /= image_features.norm(dim=-1, keepdim=True)
-        logits = 100. * image_features @ zeroshot_weights
+def print_accuracy(model, zeroshot_weights, loader):
+    with torch.no_grad():
+        top1, top5, n = 0., 0., 0.
+        for i, (images, target) in enumerate(tqdm(loader)):
+            images = images.cuda()
+            target = target.cuda()
+            
+            # predict
+            image_features = model.encode_image(images)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            logits = 100. * image_features @ zeroshot_weights
 
-        # measure accuracy
-        acc1, acc5 = accuracy(logits, target, topk=(1, 5))
-        top1 += acc1
-        top5 += acc5
-        n += images.size(0)
+            # measure accuracy
+            acc1, acc5 = accuracy(logits, target, topk=(1, 5))
+            top1 += acc1
+            top5 += acc5
+            n += images.size(0)
 
 
-top1 = (top1 / n) * 100
-top5 = (top5 / n) * 100 
+    top1 = (top1 / n) * 100
+    top5 = (top5 / n) * 100
 
-print(f"Top-1 accuracy: {top1:.2f}")
-print(f"Top-5 accuracy: {top5:.2f}")
+    print(f"Top-1 accuracy: {top1:.2f}")
+    print(f"Top-5 accuracy: {top5:.2f}")
+
+
+def run(model_name: str = 'ViT-L/14@336px', dataset_name: str = 'CIFAR100', batch_size: int = 32, num_workers: int = 2, device: str ='cuda'):
+    model, preprocess = clip.load(model_name, device)
+    dataset = get_dataset(dataset_name, preprocess)
+    loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
+    classes, templates = load_promts(dataset_name)
+    zeroshot_weights = zeroshot_classifier(model, classes, templates)
+    print_accuracy(model, zeroshot_weights, loader)
+
+
+if __name__ == '__main__':
+    fire.Fire(run)
--- scripts/run_zero_shot.sh
+++ scripts/run_zero_shot.sh
@@ -4,13 +4,13 @@ nvidia-smi
 date
 
 EVAL_PY_PATH="/home/myurachinskiy/CLIP/summer-clip/results_reproduce/zero_shot.py"
-# SUMMER_WSI_PATH="/home/myurachinskiy/WSI/summer-wsi"
+SUMMER_CLIP_PATH="/home/myurachinskiy/CLIP/summer-clip"
 
-# export PYTHONPATH="${PYTHONPATH}:${SUMMER_WSI_PATH}"
+export PYTHONPATH="${PYTHONPATH}:${SUMMER_CLIP_PATH}"
 export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 
-# cd $SUMMER_WSI_PATH || exit
+cd $SUMMER_CLIP_PATH || exit
 python -u $EVAL_PY_PATH
 # python -u -m cProfile -o GLM/scripts/program.prof $EVAL_PY_PATH --config-name eval_v1 model_vec=glm
 # python -u $REPR_PY_PATH --config-name eval_v1 +run_dir=/home/myurachinskiy/WSI/summer-wsi/outputs/2022-02-16_21-33-24
