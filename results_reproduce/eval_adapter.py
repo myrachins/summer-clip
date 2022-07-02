95531af60a5d4ce68ef00a3c363b7f3ff7a2d3e3
Maxim Rachinskiy <myurachinskiy@edu.hse.ru>
2022-07-02 18:07:50 +0300

    adapters eval

--- results_reproduce/eval_adapter.py
+++ results_reproduce/eval_adapter.py
@@ -1,43 +1,75 @@
+import logging
+import typing as tp
 from pathlib import Path
 
 import clip
-import fire
+import hydra
 import torch
+from torch.utils.data.dataloader import DataLoader
+from omegaconf import DictConfig, ListConfig, OmegaConf
 
 from results_reproduce import zero_shot
 from results_reproduce import train_adapter
 
 
-def load_adapter_model(checkpoint_dir: str, device):
-    checkpoint_dir = Path(checkpoint_dir)
+def load_checkpoint_state(clip_adapter: train_adapter.ClipAdapter, checkpoint_path: str, device: str) -> train_adapter.ClipAdapter:
+    # Since we do not save clip_model, we delete it from the model while loading
+    clip_model = clip_adapter.clip_model
+    del clip_adapter.clip_model
+    clip_adapter = clip_adapter.to(device)
 
-    model_meta = torch.load(checkpoint_dir / 'model_meta.ckpt')
-    clip_model, preprocess = clip.load(model_meta['visual_encoder_name'], device)
+    model_checkpoint = torch.load(checkpoint_path)
+    clip_adapter.load_state_dict(model_checkpoint)
 
-    model = train_adapter.ClipAdapter(clip_model, model_meta['dataset_name'], model_meta['visual_encoder_name'], model_meta['output_dim'])
-    del model.clip_model
-    model_checkpoint = torch.load(checkpoint_dir / 'model.ckpt')
-    model = model.to(device)
-    model.load_state_dict(model_checkpoint)
-    model.clip_model = clip_model
-    train_adapter.convert_model_to_fp32(model)
-    
-    return model, preprocess
+    clip_adapter.clip_model = clip_model
+    train_adapter.convert_model_to_fp32(clip_adapter)
+
+    return clip_adapter
 
 
-def run(checkpoint_dir: str, dataset_name: str, batch_size: int = 32, num_workers: int = 2, device: str = 'cuda', random_state: int = 42):
-    print(f'{dataset_name=}, {batch_size=}, {num_workers=}, {checkpoint_dir=}, {device=}')
+def eval_adapter(checkpoint_path: str, visual_encoder_name: str, adapter_fabric: train_adapter.ClipAdapterFabric,
+                 dataset_name: str, classes: tp.Optional[tp.List[str]], templates: tp.List[str],
+                 batch_size: int = 32, num_workers: int = 2, device: str = 'cuda', random_state: int = 42) -> None:
     zero_shot.set_random_state(random_state)
-    adapter_model, preprocess = load_adapter_model(checkpoint_dir, device)
-    dataset = zero_shot.get_dataset(dataset_name, preprocess)
-    loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
-    classes, templates = zero_shot.load_promts(dataset_name)
-    zeroshot_weights = zero_shot.zeroshot_classifier(adapter_model, classes, templates)
 
-    top1, top5 = zero_shot.compute_accuracy(adapter_model, zeroshot_weights, loader)
-    print(f"Top-1 accuracy: {top1:.2f}")
-    print(f"Top-5 accuracy: {top5:.2f}")
+    clip_model, preprocess = clip.load(visual_encoder_name, device)
+    dataset = zero_shot.get_dataset(dataset_name, preprocess)
+    loader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
+    classes, templates = zero_shot.load_promts(dataset_name)
+
+    clip_adapter = adapter_fabric.create_adapter(clip_model)
+    clip_adapter = load_checkpoint_state(clip_adapter, checkpoint_path, device)
+
+    zeroshot_weights = zero_shot.zeroshot_classifier(clip_adapter, classes, templates)
+    top1, top5 = zero_shot.compute_accuracy(clip_adapter, zeroshot_weights, loader)
+    logging.info(f'acc@1: {top1}')
+    logging.info(f'acc@5: {top5}')
+
+
+def load_train_config(eval_cfg: DictConfig) -> tp.Union[DictConfig, ListConfig]:
+    if eval_cfg.eval.train_config_path:
+        return OmegaConf.load(eval_cfg.eval.train_config_path)
+
+    train_config_path = Path(eval_cfg.eval.checkpoint_path).parent.parent.parent / '.hydra' / 'config.yaml'
+    return OmegaConf.load(train_config_path)
+
+
+@hydra.main(config_path='conf', config_name='eval_adapter', version_base='1.1')
+def run(cfg: DictConfig) -> None:
+    logging.info('Start!')
+    print(OmegaConf.to_yaml(cfg))
+
+    train_cfg = load_train_config(cfg)
+    visual_encoder_name = train_cfg.clip.model_name
+    adapter_fabric = hydra.utils.instantiate(train_cfg.adapter)
+
+    eval_adapter(
+        cfg.eval.checkpoint_path, visual_encoder_name, adapter_fabric, cfg.data.dataset_name,
+        cfg.prompting.classes, cfg.prompting.templates, cfg.data.batch_size, cfg.data.num_workers,
+        cfg.meta.device, cfg.meta.random_state
+    )
+    logging.info('Finish!')
 
 
 if __name__ == '__main__':
-    fire.Fire(run)
+    run()
--- scripts/run_eval_adapter.sh
+++ scripts/run_eval_adapter.sh
@@ -3,16 +3,12 @@
 nvidia-smi
 date
 
-EVAL_PY_PATH="/home/myurachinskiy/CLIP/summer-clip/results_reproduce/adapter_eval.py"
+PY_PATH="/home/myurachinskiy/CLIP/summer-clip/results_reproduce/eval_adapter.py"
 SUMMER_CLIP_PATH="/home/myurachinskiy/CLIP/summer-clip"
-
-DATASET_NAME="MNIST"
-CHECKPOINT_DIR="/home/myurachinskiy/CLIP/summer-clip/data/checkpoints/mnist-test-4/epoch_2"
-#BATCH_SIZE=512
 
 export PYTHONPATH="${PYTHONPATH}:${SUMMER_CLIP_PATH}"
 export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 
 cd $SUMMER_CLIP_PATH || exit
-python -u $EVAL_PY_PATH --dataset-name $DATASET_NAME --checkpoint-dir $CHECKPOINT_DIR
+python -u $PY_PATH
