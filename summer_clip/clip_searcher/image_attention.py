5750b1f1b43f6b61f8bbd33b570089320e760ce6
Maxim Rachinskiy <myurachinskiy@edu.hse.ru>
2022-10-16 21:32:43 +0300

    save preds + analysis

--- summer_clip/clip_searcher/image_attention.py
+++ summer_clip/clip_searcher/image_attention.py
@@ -1,4 +1,5 @@
 import typing as tp
+from pathlib import Path
 
 import clip
 import hydra
@@ -9,7 +10,7 @@ from omegaconf import DictConfig
 from summer_clip.clip_model import eval_clip
 from summer_clip.utils import hydra_utils
 from summer_clip.utils.trainer import run_trainer, BaseTrainer
-from summer_clip.clip_searcher.utils import load_labels, compute_accuracy
+from summer_clip.clip_searcher.utils import load_labels, compute_accuracy, TensorsNumpySaver
 from summer_clip.clip_searcher.cache_strategy import CacheStrategy, IndexedCacheStrategy
 
 
@@ -21,6 +22,19 @@ class ImageAttention(BaseTrainer):
         if self.cfg.cache.dataset:
             cache_dataset = hydra.utils.instantiate(self.cfg.cache.dataset)
             self.cache_labels = load_labels(cache_dataset).to(self.cfg.meta.device)
+        if self.cfg.run_saves.save_labels:
+            self.save_labels()
+
+    def setup_logger(self):
+        super().setup_logger()
+        self.gold_labels_saver = TensorsNumpySaver(Path('./gold_labels'))
+        self.cache_saver = TensorsNumpySaver(Path('./cache_ids'))
+        self.preds_saver = TensorsNumpySaver(Path('./preds_ids'))
+
+    def save_labels(self) -> None:
+        self.gold_labels_saver.save_named_tensor(self.test_labels, "test_labels")
+        if self.cache_labels is not None:
+            self.gold_labels_saver.save_named_tensor(self.cache_labels, "cache_labels")
 
     def load_test_text_features(self, device: torch.device):
         clip_model, _ = clip.load(self.cfg.clip.model_name, device, jit=False)
@@ -38,6 +52,9 @@ class ImageAttention(BaseTrainer):
         samples_inds = cache_strategy.select(image_features, image_outs)
         cache_image_features, cache_image_outs = image_features[:, samples_inds], image_outs[samples_inds]
         cache_info: tp.Dict[str, tp.Any] = dict(cache_size=cache_image_outs.shape[0])
+
+        if self.cfg.run_saves.save_cache_inds:
+            cache_info['cache_inds_path'] = str(self.cache_saver.save_tensor(samples_inds))
 
         if self.cache_labels is not None:
             cache_labels = self.cache_labels[samples_inds]
@@ -65,10 +82,17 @@ class ImageAttention(BaseTrainer):
         clip_logits = 100. * norm_test_image_features.t() @ self.test_text_features
         return clip_logits
 
+    def logits_to_preds(self, logits: torch.Tensor) -> torch.Tensor:
+        _, preds = logits.max(dim=1)
+        return preds
+
     def train_loop(self):
         clip_logits = self.compute_clip_logits()
         eval_top1, eval_top5 = compute_accuracy(clip_logits, self.test_labels)
-        self.logger.log_info(dict(acc1=eval_top1, acc5=eval_top5, type='zero_shot'))
+        zeroshot_info: tp.Dict[str, tp.Any] = dict(acc1=eval_top1, acc5=eval_top5)
+        if self.cfg.run_saves.save_preds:
+            zeroshot_info['preds_path'] = str(self.preds_saver.save_tensor(self.logits_to_preds(clip_logits)))
+        self.logger.log_info(dict(**zeroshot_info, type='zero_shot'))
 
         for cache_strategy, cache_strategy_params in hydra_utils.instantiate_all(self.cfg.cache_strategy):
             cache_image_features, cache_image_outs, cache_info = self.build_cache(
@@ -82,11 +106,14 @@ class ImageAttention(BaseTrainer):
                     for alpha in self.cfg.cache.alpha:
                         searcher_logits = clip_logits + cache_logits * alpha
                         eval_top1, eval_top5 = compute_accuracy(searcher_logits, self.test_labels)
-                        self.logger.log_info_wandb(dict(
+                        searcher_info: tp.Dict[str, tp.Any] = dict(
                             cache_strategy=cache_strategy_params, cache_value_strategy=cache_value_strategy_params,
                             cache_weights_strategy=cache_weights_strategy_params, alpha=alpha,
-                            acc1=eval_top1, acc5=eval_top5, type='searcher_result'
-                        ))
+                            acc1=eval_top1, acc5=eval_top5
+                        )
+                        if self.cfg.run_saves.save_preds:
+                            searcher_info['preds_path'] = str(self.preds_saver.save_tensor(self.logits_to_preds(searcher_logits)))
+                        self.logger.log_info_wandb(dict(**searcher_info, type='searcher_result'))
 
 
 @hydra.main(config_path='../conf', config_name='image_attention', version_base='1.2')
--- summer_clip/clip_searcher/utils.py
+++ summer_clip/clip_searcher/utils.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 import torch
+import numpy as np
 from torch.utils.data.dataset import Dataset
 
 from summer_clip.clip_adapter import train_adapter
@@ -16,3 +19,34 @@ def compute_accuracy(outputs, target, topk=(1, 5)):
         return 100. * acc / target.shape[0]
 
     return [transform_acc(acc) for acc in acc_ks]
+
+
+class FilesNamesManager:
+    def __init__(self, dir_path: Path, files_ext: str) -> None:
+        self.dir_path = dir_path.resolve()
+        self.files_ext = files_ext
+        self.dir_path.mkdir(parents=True, exist_ok=True)
+        self.counter = 0
+
+    def next_path(self) -> Path:
+        new_path = self.get_path(str(self.counter))
+        self.counter += 1
+        return new_path
+
+    def get_path(self, file_name: str) -> Path:
+        return self.dir_path / f'{file_name}{self.files_ext}'
+
+
+class TensorsNumpySaver:
+    def __init__(self, dir_path: Path) -> None:
+        self.files_names_manager = FilesNamesManager(dir_path, files_ext='.npy')
+
+    def save_tensor(self, tensor: torch.Tensor) -> Path:
+        tensor_path = self.files_names_manager.next_path()
+        return self.save_named_tensor(tensor, tensor_path.with_suffix('').name)
+
+    def save_named_tensor(self, tensor: torch.Tensor, file_name: str) -> Path:
+        tensor_np = tensor.cpu().numpy()
+        tensor_path = self.files_names_manager.get_path(file_name)
+        np.save(tensor_path, tensor_np)
+        return tensor_path
