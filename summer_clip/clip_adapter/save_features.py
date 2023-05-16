2e6df19d6b6c34cef58ab5de5db02f385647e671
Maxim Rachinskiy <myurachinskiy@edu.hse.ru>
2023-05-16 05:30:27 +0300

    clip-search

--- summer_clip/clip_adapter/save_features.py
+++ summer_clip/clip_adapter/save_features.py
@@ -37,29 +37,41 @@ def calculate_image_features(model, loader, device):
     return images, indexes
 
 
-def save_features(model_name: str, dataset_cfg: DictConfig, output_path: str, batch_size: int, num_workers: int,
-                  device: tp.Optional[str], random_state: int) -> None:
-    eval_clip.set_random_state(random_state)
+def save_image_outs(cfg, clip_model, image_features, dataset, device):
+    print('Computing outs...')
+    clip_classes = cfg.prompting.classes or dataset.classes
+    text_features = eval_clip.zeroshot_classifier(clip_model, clip_classes, cfg.prompting.templates).to(device)
+    image_features = image_features.to(device)
+    image_features = image_features / image_features.norm(dim=0, keepdim=True)
+    image_outs = image_features.t() @ text_features
+    torch.save(image_outs, 'train_image_outs.pt')
 
-    device = device or ('cuda' if torch.cuda.is_available() else 'cpu')
-    clip_model, preprocess = clip.load(model_name, device, jit=False)
+
+def save_features(cfg: DictConfig, dataset_cfg: DictConfig, output_path: str, save_outs: bool) -> None:
+    eval_clip.set_random_state(cfg.meta.random_state)
+
+    device = cfg.meta.device or ('cuda' if torch.cuda.is_available() else 'cpu')
+    clip_model, preprocess = clip.load(cfg.clip.model_name, device, jit=False)
     dataset = hydra.utils.instantiate(dataset_cfg, transform=preprocess)
     indexed_dataset = IndexedDataset(dataset)
-    indexed_loader = DataLoader(indexed_dataset, batch_size=batch_size, num_workers=num_workers)
+    indexed_loader = DataLoader(indexed_dataset, batch_size=cfg.data.batch_size, num_workers=cfg.data.num_workers)
 
     images, indexes = calculate_image_features(clip_model, indexed_loader, device)
     assert indexes == list(range(len(indexes))), "Indexes should have consequent order"
     torch.save(images, output_path)
+
+    if save_outs:
+        save_image_outs(cfg, clip_model, images, dataset, device)
 
 
 @hydra.main(config_path='../conf', config_name='save_features', version_base='1.1')
 def run(cfg: DictConfig) -> None:
     logging.info('Start!')
     print(OmegaConf.to_yaml(cfg))
-    save_features(
-        cfg.clip.model_name, cfg.dataset, cfg.data.output_path, cfg.data.batch_size,
-        cfg.data.num_workers, cfg.meta.device, cfg.meta.random_state
-    )
+    if cfg.get('train_dataset') is not None:
+        save_features(cfg, dataset_cfg=cfg.train_dataset, output_path='train_image_features.pt', save_outs=cfg.save_train_outs)
+    if cfg.get('test_dataset') is not None:
+        save_features(cfg, dataset_cfg=cfg.test_dataset, output_path='test_image_features.pt', save_outs=False)
     logging.info('Finish!')
 
 
